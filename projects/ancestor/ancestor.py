d0414774b7653fb72db597af9778c8d0a6df68cc
Natalia Beckstead <toosmiravilla@gmail.com>
2020-08-11 13:50:53 -0600

    added another solution for ancestor using bfs

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,4 @@
+from util import Queue
 
 def earliest_ancestor(ancestors, starting_node):
     graph = {}
@@ -5,14 +6,31 @@ def earliest_ancestor(ancestors, starting_node):
         if i[1] not in graph:
             graph[i[1]] = []
         graph[i[1]].append(i[0])
-    print(graph)
     if starting_node not in graph:
         return -1
-
-    ancestor = starting_node
+########## 1st solution ###############
+    # ancestor = starting_node
     
-    while ancestor in graph:
-       ancestor = graph[ancestor][0]
-    print(ancestor)
-    return(ancestor)
+    # while ancestor in graph:
+    #    ancestor = graph[ancestor][0]
+    # return(ancestor)
 
+########## 2nd solution ###############
+    q = Queue()
+    visited = set()
+    path = [starting_node]
+    q.enqueue(path)
+
+    while q.size() > 0:
+        current_path = q.dequeue()
+        current_node = current_path[-1]
+        if current_node not in graph:
+            return current_path[-1]
+        if current_node not in visited:
+            visited.add(current_node)
+            path_copy = list(current_path)
+            path_copy.append(graph[current_node][0])
+            q.enqueue(path_copy)
+
+# Both solutions passing test
+earliest_ancestor([(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)], 6)
